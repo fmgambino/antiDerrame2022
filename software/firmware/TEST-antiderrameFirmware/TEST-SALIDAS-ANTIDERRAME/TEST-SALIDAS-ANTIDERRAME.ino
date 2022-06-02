e6a9a4e3f11a32dcd3f524331bc48eca65f29942
fmgambino <fernando.m.gambino@gmail.com>
2022-06-02 16:19:47 -0300

    Update Files.

--- software/firmware/TEST-antiderrameFirmware/TEST-SALIDAS-ANTIDERRAME/TEST-SALIDAS-ANTIDERRAME.ino
+++ software/firmware/TEST-antiderrameFirmware/TEST-SALIDAS-ANTIDERRAME/TEST-SALIDAS-ANTIDERRAME.ino
@@ -2,34 +2,30 @@ int relay1 = 13;
 int relay2 = 9;
 int mosfet1 = 10;
 int mosfet2 = 12;
-int estado =0;
+
+int stemp = 35;
+
 void setup() {
-  // put your setup code here, to run once:
+  // salidas
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(mosfet1, OUTPUT);
   pinMode(mosfet2, OUTPUT);
+  // entradas
   
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    digitalWrite(relay1,LOW);
-    delay(500);
-    digitalWrite(relay2,LOW);
-    delay(500);
-    digitalWrite(mosfet1,LOW);
-    delay(500);
+    digitalWrite(relay1,LOW);    
+    digitalWrite(relay2,LOW);   
+    digitalWrite(mosfet1,LOW);    
     digitalWrite(mosfet2,LOW);    
-    delay(500);
-    digitalWrite(relay1,HIGH);
-    delay(500);
-    digitalWrite(relay2,HIGH);
-    delay(500);
-    digitalWrite(mosfet1,HIGH);
-    delay(500);
-    digitalWrite(mosfet2,HIGH);
-    delay(500);
+    
 
+    Serial.println(analogRead(stemp));
+    
+    
    
 }
--- software/firmware/TEST-antiderrameFirmware/TEST-antiderrameFirmware.ino
+++ software/firmware/TEST-antiderrameFirmware/TEST-antiderrameFirmware.ino
@@ -1,42 +1,33 @@
-//#include <Wire.h>   // incluye libreria para interfaz I2C
-//#include <RTClib.h>   // incluye libreria para el manejo del modulo RTC
-//#include <OneWire.h>
 
-//#include <DallasTemperature.h>
 
-//RTC_DS3231 rtc;     // crea objeto del tipo RTC_DS3231
+// ENTRADAS
 
-//ENTRADAS
-/*
 int bWiFi = 23;
 
-int sCap1 =25 ;
-int sCap2 =26 ;
-int sCap3 =27 ;
-float sOptico =34 ;
-//float sTemp = 35;
-const int oneWireBus = 35;
-//OneWire oneWire(oneWireBus);
-//DallasTemperature sensors (&oneWire);
+int sCap1   = 25;
+int sCap2   = 26;
+int sCap3   = 27;
+int sOptico = 34;
+int sTemp   = 35;
 
 int sAmp1 = 32; // Entrada 4-20 mA
 int sAmp2 = 33; // Entrada 4-20 mA
-*/
-//SALIDAS
 
-int relay1 = 13;
-int relay2 = 9;
+// SALIDAS
+
+int relay1  = 13;
+int relay2  = 9;
 int mosfet1 = 10;
-int mosfet2 = 11;
+int mosfet2 = 12;
 
 
-
+int estadoCap1 = 0;
+int estadoCap2 = 0;
+int estadoCap3 = 0;
 //************************************
 //***** DECLARACION FUNCIONES ********
 //************************************
 
-//void rtcEG();
-/*
 void Cap1();
 void Cap2();
 void Cap3();
@@ -44,9 +35,10 @@ void Cap3();
 void Optico();
 void Temp();
 
-void Amp1();
-void Amp2();
-*/
+void Amp1(); // entrada 4-20ma
+void Amp2(); // entrada 4-20ma
+
+// funciones de salidas
 void Rele1();
 void Rele2();
 
@@ -54,44 +46,33 @@ void MosFet1();
 void MosFet2();
 
 
-void setup () {
- Serial.begin(115200);    // inicializa comunicacion serie a 9600 bps
-// sensors.begin();
- Serial.println("DHTxx test by EG!");
-/*
- pinMode(sCap1, INPUT);
- pinMode(sCap2, INPUT);
- pinMode(sCap3, INPUT);
+void setup()
+{
+  Serial.begin(115200); // inicializa comunicacion serie a 9600 bps
+  Serial.println("inicio");
 
- pinMode(sOptico, INPUT);
+  pinMode(sCap1, INPUT);
+  pinMode(sCap2, INPUT);
+  pinMode(sCap3, INPUT);
 
-*/
- pinMode(relay1, OUTPUT);
- pinMode(relay2, OUTPUT);
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(mosfet1, OUTPUT);
+  pinMode(mosfet2, OUTPUT);
 
- pinMode(mosfet1, OUTPUT);
- pinMode(mosfet2, OUTPUT);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  digitalWrite(mosfet1, LOW);
+  digitalWrite(mosfet2, LOW);
+}
 
-/*
- if (! rtc.begin()) {       // si falla la inicializacion del modulo
- Serial.println("Modulo RTC no encontrado !");  // muestra mensaje de error
- while (1);         // bucle infinito que detiene ejecucion del programa
- }
- //rtc.adjust(DateTime(__DATE__, __TIME__));  // funcion que permite establecer fecha y horario
-            // al momento de la compilacion. Comentar esta linea
-            // y volver a subir el codigo para funcionamiento normal
-  */
-}           
+void loop()
+{
+  // Wait a few seconds between measurements.
+  delay(2000);
 
-void loop () {
- // Wait a few seconds between measurements.
-   delay(2000);
+  Serial.println("LECTURAS ENTRADAS ");
 
-   //rtcEG();
-
-
-   Serial.println("LECTURAS ENTRADAS ");
-/*
   Cap1();
   Cap2();
   Cap3();
@@ -101,112 +82,76 @@ void loop () {
 
   Amp1();
   Amp2();
-*/
-   digitalWrite(relay1,HIGH);
-   delay(500);
-   digitalWrite(relay1,LOW);
-   delay(500);
-
-   digitalWrite(relay2,LOW);
-   delay(500);
-   digitalWrite(relay2,HIGH);
-   delay(500);
-
-   digitalWrite(mosfet1,LOW);
-   delay(500);
-   digitalWrite(mosfet1,HIGH);
-   delay(500);
-
-   digitalWrite(mosfet2,LOW);
-   delay(500);
-   digitalWrite(mosfet2,HIGH);
-   delay(500);
-}
-/*
-void rtcEG(){
-
- DateTime fecha = rtc.now();      // funcion que devuelve fecha y horario en formato
-            // DateTime y asigna a variable fecha
- Serial.print(fecha.day());     // funcion que obtiene el dia de la fecha completa
- Serial.print("/");       // caracter barra como separador
- Serial.print(fecha.month());     // funcion que obtiene el mes de la fecha completa
- Serial.print("/");       // caracter barra como separador
- Serial.print(fecha.year());      // funcion que obtiene el año de la fecha completa
- Serial.print(" ");       // caracter espacio en blanco como separador
- Serial.print(fecha.hour());      // funcion que obtiene la hora de la fecha completa
- Serial.print(":");       // caracter dos puntos como separador
- Serial.print(fecha.minute());      // funcion que obtiene los minutos de la fecha completa
- Serial.print(":");       // caracter dos puntos como separador
- Serial.println(fecha.second());    // funcion que obtiene los segundos de la fecha completa
  
- delay(1000);         // demora de 1 segundo
-
 }
-*/
 
-
-//SENSOR OPTICO 1 - NIVEL BAJO
-void Cap1(){
-  int estadoCap1=0;
+// SENSOR OPTICO 1 - NIVEL BAJO
+void Cap1()
+{  
   estadoCap1 = digitalRead(sCap1);
-
-  if (estadoCap1 == LOW) {
+  if (estadoCap1 == HIGH)
+  {
     // turn LED on:
-    Serial.println("NIVEL-BAJO!!! ");
+    Serial.println("cap 1 activado ");
     delay(10);
+    digitalWrite(relay1, HIGH);  
   }
-
+  else{
+    Serial.println("cap 1 Apagado ");
+    digitalWrite(relay1, LOW);
+    }
 }
 
-void Cap2(){
-  int estadoCap2=0;
-
+void Cap2()
+{
+  
   estadoCap2 = digitalRead(sCap2);
-
-  if (estadoCap2 == LOW) {
+  if (estadoCap2 == HIGH)
+  {
     // turn LED on:
-    Serial.println("NIVEL-MEDIO!!! ");
-    delay(10);
+    Serial.println("cap 2 activado ");
+    digitalWrite(relay2, HIGH);  
   }
+  else{
+    Serial.println("cap 2 Apagado ");
+    digitalWrite(relay2, LOW);
+    }
 }
 
-void Cap3(){
-    int estadoCap3=0;
-    estadoCap3 = digitalRead(sCap3);
-
-  if (estadoCap3 == LOW) {
+void Cap3()
+{
+  
+  estadoCap3 = digitalRead(sCap3);
+  if (estadoCap3 == HIGH)
+  {
     // turn LED on:
-    Serial.println("NIVEL-ALTO!!! ");
-    delay(10);
+    Serial.println("cap 3 activado ");
+    digitalWrite(mosfet1, HIGH);
   }
+  else{
+    Serial.println("cap 3 Apagado ");
+    digitalWrite(mosfet1, LOW);
+    }
 }
 
-void Optico(){
+void Optico()
+{
+  Serial.print("Valor Sensor Optico     : ");
+  Serial.println(analogRead(sOptico));
+  float volts = analogRead(sOptico)*(3/4095);  
+  Serial.println(analogRead(volts));
 }
 
-void Temp(){
-    //Leer la temperatura
-/*
-  Serial.print("Mandando comandos a los sensores ");
-  sensors.requestTemperatures();
-  //Lectura en grados celsius
-
-  float temperatureC = sensors.getTempCByIndex(0);
-
- //Escribir los datos en el monitor de serie
-
-  Serial.print("Temperatura sensor : ");
-  Serial.print(temperatureC);
-  Serial.println("°C");
-
-  // Lectura de la temperatura cada 100 ms
-
-  delay(100);
-  */
+void Temp()
+{
+  Serial.print("Valor Sensor Temperatura: ");
+  Serial.println(analogRead(sTemp));
 }
 
-void Amp1(){
+void Amp1()
+{
 }
 
-void Amp2(){
+void Amp2()
+{
 }
